3d897782a88e8b5271395ab86431306f04ed50f1
sibmaks <sibmaks@vk.com>
2025-04-21 10:54:19 +0300

    Patch proxy for linux

--- perf/src/main/java/info/ragozin/util/socketstifler/LatencyProxy.java
+++ perf/src/main/java/info/ragozin/util/socketstifler/LatencyProxy.java
@@ -1,26 +1,40 @@
 package info.ragozin.util.socketstifler;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.management.MBeanServer;
+import javax.management.MXBean;
+import javax.management.ObjectName;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.management.ManagementFactory;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.LockSupport;
 
-import javax.management.MXBean;
-import javax.management.ObjectName;
-
-import org.gridkit.internal.com.jcraft.jsch.Packet;
-
 public class LatencyProxy {
+    private static final Log LOG = LogFactory.getLog(LatencyProxy.class);
+
+    private final int packetSize = 16 << 10;
+    private final AtomicLong connectionCount = new AtomicLong();
+    private final AtomicLong client2server = new AtomicLong();
+    private final AtomicLong server2client = new AtomicLong();
+    private final Map<Socket, AtomicInteger> closeCounter = new ConcurrentHashMap<>();
+    private volatile long latency = 0;
 
     public static LatencyProxy start(SocketAddress accept, SocketAddress forward) throws Exception {
         LatencyProxy proxy = new LatencyProxy();
-        int inPort = ((InetSocketAddress)accept).getPort();
-        int outPort = ((InetSocketAddress)forward).getPort();
+        int inPort = ((InetSocketAddress) accept).getPort();
+        int outPort = ((InetSocketAddress) forward).getPort();
         ObjectName name = ObjectName.getInstance("info.ragozin:type=SocketPoxy,src=" + inPort + ",fwd=" + outPort);
 
         ServerSocket sock = new ServerSocket();
@@ -29,30 +43,15 @@ public class LatencyProxy {
         AcceptThread acc = proxy.new AcceptThread(sock, forward);
         acc.start();
 
+        MBeanServer platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
         try {
-            ManagementFactory.getPlatformMBeanServer().unregisterMBean(name);
-        } catch (Exception e) {
+            platformMBeanServer.unregisterMBean(name);
+        } catch (Exception ignored) {
         }
 
-        ManagementFactory.getPlatformMBeanServer().registerMBean(proxy.getMBean(), name);
+        platformMBeanServer.registerMBean(proxy.getMBean(), name);
 
         return proxy;
-    }
-
-    private volatile long latency = 0;
-
-    private int packetSize = 16 << 10;
-
-    private AtomicLong connectionCount = new AtomicLong();
-    private AtomicLong client2server = new AtomicLong();
-    private AtomicLong server2client = new AtomicLong();
-
-    public long getLatency() {
-        return latency;
-    }
-
-    public void setLatency(long latency) {
-        this.latency = latency;
     }
 
     private ProxyMBean getMBean() {
@@ -86,6 +85,20 @@ public class LatencyProxy {
     }
 
 
+    @MXBean
+    public interface ProxyMBean {
+
+        long getClient2Server();
+
+        long getServer2Client();
+
+        long getTotalConnectionCount();
+
+        long getLatencyMicros();
+
+        void setLatencyMicros(long latencyMicrocs);
+    }
+
     private class AcceptThread extends Thread {
 
         private final ServerSocket socket;
@@ -101,26 +114,26 @@ public class LatencyProxy {
         @Override
         public void run() {
             try {
-                while(true) {
+                while (!Thread.interrupted()) {
                     Socket clientSide = socket.accept();
                     try {
-                        if (clientSide != null) {
-                            Socket serverSide = new Socket();
-                            serverSide.connect(forward);
+                        Socket serverSide = new Socket();
+                        serverSide.connect(forward);
 
-                            ForwardThread a = new ForwardThread(clientSide, serverSide, client2server);
-                            ForwardThread b = new ForwardThread(serverSide, clientSide, server2client);
-                            a.start();
-                            b.start();
-                            connectionCount.incrementAndGet();
-                        }
-                    }
-                    catch (IOException e) {
-                        // ignore
+                        closeCounter.put(clientSide, new AtomicInteger(2));
+                        closeCounter.put(serverSide, new AtomicInteger(2));
+
+                        ForwardThread a = new ForwardThread(clientSide, serverSide, client2server);
+                        ForwardThread b = new ForwardThread(serverSide, clientSide, server2client);
+                        a.start();
+                        b.start();
+                        connectionCount.incrementAndGet();
+                    } catch (IOException e) {
+                        LOG.error("Accept failed", e);
                     }
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                LOG.error("Accept failed", e);
             }
         }
     }
@@ -142,54 +155,40 @@ public class LatencyProxy {
 
         @Override
         public void run() {
-            while(true) {
-                try {
-                    byte[] buf = new byte[packetSize];
+            try (InputStream inputStream = input.getInputStream();
+                 OutputStream outputStream = output.getOutputStream()) {
+                byte[] buf = new byte[packetSize];
 
-                    int n = input.getInputStream().read(buf);
-
-                    if (n > 0) {
-                        if (latency > 0) {
-                            LockSupport.parkNanos(latency);
-                        }
-                        byteCounter.addAndGet(n);
-                        output.getOutputStream().write(buf, 0, n);
+                int n;
+                while ((n = inputStream.read(buf)) > 0) {
+                    if (latency > 0) {
+                        LockSupport.parkNanos(latency);
                     }
-                    else if (n < 0) {
-                        output.shutdownOutput();
-                        if (input.isOutputShutdown()) {
-                            input.close();
-                            output.close();
-                        }
-                        break;
-                    }
-
-                } catch (IOException e) {
-                    try {
-                        input.close();
-                    } catch (IOException e1) {
-                    }
-                    try {
-                        output.close();
-                    } catch (IOException e1) {
-                    }
-                    break;
+                    byteCounter.addAndGet(n);
+                    outputStream.write(buf, 0, n);
                 }
+            } catch (IOException e) {
+                LOG.error("Forwarding exception", e);
+            } finally {
+                silenceClose(input);
+                silenceClose(output);
             }
         }
-    }
 
-    @MXBean
-    public static interface ProxyMBean {
-
-        public long getClient2Server();
-
-        public long getServer2Client();
-
-        public long getTotalConnectionCount();
-
-        public long getLatencyMicros();
-
-        public void setLatencyMicros(long latencyMicrocs);
+        private void silenceClose(Socket socket) {
+            if (socket == null || socket.isClosed()) {
+                return;
+            }
+            AtomicInteger counter = closeCounter.getOrDefault(socket, new AtomicInteger(1));
+            if (counter.decrementAndGet() > 0) {
+                return;
+            }
+            closeCounter.remove(socket);
+            try {
+                socket.close();
+            } catch (IOException e) {
+                LOG.warn(String.format("Silence close exception: %s", socket), e);
+            }
+        }
     }
 }
